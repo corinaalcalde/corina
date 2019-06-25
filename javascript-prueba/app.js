3c43294af376609182ceb551fade4cd5d084b972
corinaalcalde <48772510+corinaalcalde@users.noreply.github.com>
2019-06-25 14:12:31 -0500

    Add files via upload

---
+++ javascript-prueba/app.js
@@ -0,0 +1,25 @@
+// 1
+let ageDead = "No morirás sino hasta los 70 años ",
+    mayorLogro = "Y tu mayor logro será:Trabajar ",
+    dondeVivire = "De adulto, vivirás en Lima ",
+    trabajo = "trabajando en Desarrollo web",
+    sueldo = "estarás ganando $5 000 al mes "
+console.log(ageDead + mayorLogro + dondeVivire + trabajo + sueldo)
+
+// 2
+let naci = 2000,
+    futuro = 2050,
+    edadPosible = futuro - naci,
+    segundaOpcion = edadPosible - 1
+console.log("tendre " + edadPosible + " o " + segundaOpcion + " En el año " + futuro)    
+
+
+//3
+let edad = 18,
+    edadCuandoMuera = 70,
+    maxHelados = 5,
+    heladosTotal = (edadCuandoMuera - edad) * (5* 365)
+console.log("Si tuviera helados de por vida me comería " + heladosTotal )
+
+// 4
+let radio = 10
